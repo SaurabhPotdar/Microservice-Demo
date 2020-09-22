dd2ded44073175814bf5debd4b0fc7a58b9888d4
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2020-09-22 13:36:05 +0530

    getUserRatings done in rating service

--- rating-service-1/src/main/java/com/cg/ratingservice/repository/RatingRepository.java
+++ rating-service-1/src/main/java/com/cg/ratingservice/repository/RatingRepository.java
@@ -1,6 +1,9 @@
 package com.cg.ratingservice.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cg.ratingservice.dto.Rating;
@@ -8,4 +11,6 @@ import com.cg.ratingservice.dto.Rating;
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
 
+	public List<Rating> findByUserId(@Param("userId") int userId);
+	
 }
--- rating-service-2/src/main/java/com/cg/ratingservice/repository/RatingRepository.java
+++ rating-service-2/src/main/java/com/cg/ratingservice/repository/RatingRepository.java
@@ -1,11 +1,16 @@
 package com.cg.ratingservice.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cg.ratingservice.dto.Rating;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
+	
+	public List<Rating> findByUserId(@Param("userId") int userId);
 
 }
